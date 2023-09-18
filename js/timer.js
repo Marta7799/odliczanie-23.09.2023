387e88c103d9cdb134e8113df7774e16474f3cda
Mart <marta.nikiel@yahoo.com>
2023-09-18 15:44:53 +0200

    ready counter to 23.09.2023

--- js/timer.js
+++ js/timer.js
@@ -1,23 +1,15 @@
-// import flatpickr from "flatpickr";
-// import "flatpickr/dist/flatpickr.min.css";
-// import Notiflix from "notiflix";
-
-function makeTimer() {
-  var endTime = new Date("23 September 2023 16:00:00 GMT+01:00");
-  endTime = Date.parse(endTime) / 1000;
-
-  var now = new Date();
-  now = Date.parse(now) / 1000;
-
-  var timeLeft = endTime - now;
-
-  var days = Math.floor(timeLeft / 86400);
-  var hours = Math.floor((timeLeft - days * 86400) / 3600);
-  var minutes = Math.floor((timeLeft - days * 86400 - hours * 3600) / 60);
+function countDown() {
+  var last = new Date("23 September 2023 16:00:00");
+  var last = Date.parse(last) / 1000;
+  var current = new Date();
+  var current = Date.parse(current) / 1000;
+  var leftTime = last - current;
+  var days = Math.floor(leftTime / 86400);
+  var hours = Math.floor((leftTime - days * 86400) / 3600);
+  var minutes = Math.floor((leftTime - days * 86400 - hours * 3600) / 60);
   var seconds = Math.floor(
-    timeLeft - days * 86400 - hours * 3600 - minutes * 60
+    leftTime - days * 86400 - hours * 3600 - minutes * 60
   );
-
   if (hours < "10") {
     hours = "0" + hours;
   }
@@ -27,13 +19,11 @@ function makeTimer() {
   if (seconds < "10") {
     seconds = "0" + seconds;
   }
-
   $("#days").html(days + "<span>Days</span>");
   $("#hours").html(hours + "<span>Hours</span>");
   $("#minutes").html(minutes + "<span>Minutes</span>");
   $("#seconds").html(seconds + "<span>Seconds</span>");
 }
-
 setInterval(function () {
-  makeTimer();
-}, 1000);
+  countDown();
+}, 0);
